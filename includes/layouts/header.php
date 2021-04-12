36508fbb948e1f2b8fe6ad109ec4e065a6a59580
Ben <mineeraft@gmail.com>
2021-04-12 20:27:49 +1200

    Add various favicon sizes

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -9,8 +9,18 @@ require_once(__DIR__ . '/../config.php')
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Key Homes<?php echo (!empty($title)) ? " | {$title}" : '' ?></title>
-  <link rel="shortcut icon" href="<?php echo $site_root ?>/static/img/favicon.png" type="image/x-icon">
 
+  <!-- Icons -->
+  <link rel="shortcut icon" href="<?php echo $site_root ?>/static/img/logos/icon32.png" type="image/x-icon"
+    sizes="32x32">
+  <link rel="shortcut icon" href="<?php echo $site_root ?>/static/img/logos/icon128.png" type="image/x-icon"
+    sizes="128x128">
+  <link rel="shortcut icon" href="<?php echo $site_root ?>/static/img/logos/icon192.png" type="image/x-icon"
+    sizes="192x192">
+  <link rel="shortcut icon" href="<?php echo $site_root ?>/static/img/logos/icon512.png" type="image/x-icon"
+    sizes="512x512">
+
+  <!-- CSS -->
   <link rel="stylesheet" href="<?php echo $site_root ?>/static/css/theme.css">
 </head>
 
