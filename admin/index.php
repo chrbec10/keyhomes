d3b913cea7031063fa0691332096873ceea967ca
Aurorah Harmony <mineeraft@gmail.com>
2021-04-12 12:11:09 +1200

    Init Admin dash

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$title = "Admin"; //The Page Title
+require_once('../includes/layouts/header.php'); //Gets the header
+require_once('../includes/db.php'); //Connect to the database
+?>
+
+<h1>this is the admin page</h1>
+
+<?php
+require_once('../includes/layouts/footer.php'); //Gets the footer
+?>
