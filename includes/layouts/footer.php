b222107b36deac82db8930ae5b4cfaf95c0740ba
Aurorah Harmony <mineeraft@gmail.com>
2021-04-13 11:16:23 +1200

    Init Footer

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -1,3 +1,15 @@
+<div class="container-fluid bg-dark text-white mt-2 py-3">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-6 text-md-start">Title</div>
+      <div class="col-md-6 text-md-end">Social</div>
+    </div>
+    <hr>
+    <div>About, Contact etc</div>
+    <div class="text-center text-muted">&copy; Key Homes , Website by Christ Becker & Ben Mitchell</div>
+  </div>
+</div>
+
 <script src="<?php echo $site_root ?>/static/js/bootstrap.bundle.js"></script>
 
 </body>
