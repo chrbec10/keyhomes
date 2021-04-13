4206bcea3fd9e8341c478a917a9f5b740d2e6105
Aurorah Harmony <mineeraft@gmail.com>
2021-04-13 12:13:56 +1200

    Add comment to title

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -11,6 +11,8 @@ $is_admin = false;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Only add title if variable is not empty when this file is parsed -->
   <title>Key Homes<?php echo (!empty($title)) ? " | {$title}" : '' ?></title>
 
   <!-- Icons -->
