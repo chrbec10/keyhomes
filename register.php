9ceec5d28b96b3abc8fd0e84b9f1c91f0f1f889a
Ben <mineeraft@gmail.com>
2021-04-23 13:25:23 +1200

    Integrate Register form

--- register.php
+++ register.php
@@ -1,6 +1,4 @@
 <?php
-
-$title = 'Register'; //Page title
 require_once('./includes/db.php'); //Connect to the database
 
 //Initializing variables with empty values
@@ -10,16 +8,15 @@ $username_err = $password_err = $pass_confirm_err = '';
 
 //Process registration data when form is submitted
 //Fetches 'username', 'password', and 'pass_confirm'
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter a username.";
-    }
-    else {
+    } else {
         //Prepare a SELECT statement to check for existing users
         $sql = "SELECT user_ID FROM users WHERE username = :username";
 
-        if ($stmt = $pdo->prepare($sql)){ 
+        if ($stmt = $pdo->prepare($sql)) {
             //Bind variables to the statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
 
@@ -27,15 +24,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_username = trim($_POST["username"]);
 
             //Attempt our prepared statement
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
+            if ($stmt->execute()) {
+                if ($stmt->rowCount() == 1) {
                     $username_err = "A user with this username already exists.";
-                }
-                else {
+                } else {
                     $username = trim($_POST["username"]);
                 }
-            }
-            else {
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
             unset($stmt);
@@ -43,34 +38,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     //Validating password
-    if(empty(trim($_POST["password"]))){
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
-    }
-    elseif(strlen(trim($_POST["password"])) < 6){
+    } elseif (strlen(trim($_POST["password"])) < 6) {
         $password_err = "Your password must be more than 6 characters long.";
-    }
-    else {
+    } else {
         $password = trim($_POST["password"]);
     }
 
     //Validating password confirmation
-    if(empty(trim($_POST["pass_confirm"]))){
+    if (empty(trim($_POST["pass_confirm"]))) {
         $pass_confirm_err = "Please confirm your password.";
-    }
-    else {
+    } else {
         $pass_confirm = trim($_POST["pass_confirm"]);
-        if(empty($password_err) && ($password != $pass_confirm)){
+        if (empty($password_err) && ($password != $pass_confirm)) {
             $pass_confirm_err = "Password and confirmation do not match.";
         }
     }
 
     //Check for input errors before adding to database
-    if(empty($username_err) && empty($password_err) && empty($pass_confirm_err)){
+    if (empty($username_err) && empty($password_err) && empty($pass_confirm_err)) {
 
         //Prepare to insert username into database
         $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
 
-        if($stmt = $pdo->prepare($sql)){
+        if ($stmt = $pdo->prepare($sql)) {
             //Bind variables to the statment as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
@@ -80,11 +72,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_password = password_hash($password, PASSWORD_DEFAULT); //Hashes the password
 
             //Try our prepared statement
-            if($stmt->execute()){
+            if ($stmt->execute()) {
                 //Redirect to the homepage
                 header("location: index.php");
-            }
-            else{
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
             unset($stmt);
@@ -92,4 +83,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     unset($pdo);
 }
+
+$title = "Register"; //The Page Title
+require_once('./includes/layouts/header.php');
+?>
+<div class="container content-top-padding">
+
+
+  <div class="row justify-content-center my-5">
+    <div class="col-md-6 col-lg-5 ">
+      <h1>Register</h1>
+
+
+      <form action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method="post">
+        <div class="form-group mb-1">
+          <label>Username</label>
+          <input type="text" name="username"
+            class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+            value="<?php echo $username; ?>">
+          <span class="invalid-feedback"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group mb-1">
+          <label>Password</label>
+          <input type="password" name="password"
+            class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+            value="<?php echo $password; ?>">
+          <span class="invalid-feedback"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group mb-3">
+          <label>Confirm Password</label>
+          <input type="password" name="pass_confirm"
+            class="form-control <?php echo (!empty($pass_confirm_err)) ? 'is-invalid' : ''; ?>"
+            value="<?php echo $pass_confirm; ?>">
+          <span class="invalid-feedback"><?php echo $pass_confirm_err; ?></span>
+        </div>
+
+        <input type="submit" class="btn btn-primary w-100 mb-1" value="Submit">
+
+        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<?php
+require_once('./includes/layouts/footer.php'); //Gets the footer
 ?>
