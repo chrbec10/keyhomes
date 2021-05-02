0748bd73fcfc70f085d21e560bba69d053a403a5
Ben Mitchell <mineeraft@gmail.com>
2021-05-03 11:11:57 +1200

    move search block to a module

--- listings.php
+++ listings.php
@@ -11,24 +11,7 @@ require_once('./includes/db.php'); //Connect to the database
     <h1>
       <?php echo !empty($_GET['city']) ? "" : 'All ' ?>Listings<?php echo !empty($_GET['city']) ? " in {$_GET['city']}" : '' ?>
     </h1>
-    <div class="row">
-      <div class="col-md-8 col-lg-9">
-        <div class="card card-body py-4">
-          <div class="container-fluid">
-
-            <?php require('./includes/search/results.php') ?>
-
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 col-lg-3 mb-3 mb-md-0">
-        <div class="card card-body">
-
-          <?php require('./includes/search/filter.php') ?>
-
-        </div>
-      </div>
-    </div>
+    <?php require('./includes/search/search-block.php') ?>
   </div>
 </div>
 
