4bbe433e046bd35cab0036989369cf6bdcfb1402
Ben Mitchell <mineeraft@gmail.com>
2021-05-03 11:20:53 +1200

    Add modules to wishlist page

--- listings.php
+++ listings.php
@@ -11,7 +11,9 @@ require_once('./includes/db.php'); //Connect to the database
     <h1>
       <?php echo !empty($_GET['city']) ? "" : 'All ' ?>Listings<?php echo !empty($_GET['city']) ? " in {$_GET['city']}" : '' ?>
     </h1>
+
     <?php require('./includes/search/search-block.php') ?>
+
   </div>
 </div>
 
