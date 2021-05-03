1c77c59d9b79551166e2865fdef0185e0a2db0a0
keybecker <82342623+keybecker@users.noreply.github.com>
2021-05-03 13:10:14 +1200

    New listing validation
    Post to database not implemented yet, but validation is in place for all fields.

--- admin/index.php
+++ admin/index.php
@@ -5,10 +5,18 @@ require_once('../includes/layouts/header.php'); //Gets the header
 require_once('../includes/db.php'); //Connect to the database
 ?>
 <div style="padding-top:50px">;
+
     <?php
+
+    //Select properties, joining on the agent information by ID
     $sql = "SELECT * FROM property JOIN agent ON agent.agent_ID = property.agent_ID";
+
+    //If our query is successful
     if($result = $pdo->query($sql)){
         if($result->rowCount() > 0){
+
+            //Generate table header
+            //TODO View/Edit/Delete cell + New button
             echo "<table class='table table-bordered table-striped'>";
                 echo "<thead>";
                     echo "<tr>";
@@ -20,20 +28,31 @@ require_once('../includes/db.php'); //Connect to the database
                     echo "</tr>";
                 echo "</thead>";
                 echo "<tbody>";
-                while ($row = $result->fetch()){
+
+                //Display up to the 10 most recent entries in simple form
+                $count = 0;
+                while (($row = $result->fetch())&& $count < 10){
                     echo "<tr>";
+                        //ID number
                         echo "<td>".$row['property_ID']."</td>";
-                        echo "<td>".$row['streetNum'].' '.$row['street'].', '.$row['city'].' '.$row['postcode']."</td>";
+                        //Address + link to listing
+                        echo "<td><a href='../listing.php?id=".$row['property_ID']."'>".$row['streetNum'].' '.$row['street'].', '.$row['city'].' '.$row['postcode']."</a></td>";
+                        //Sale type
                         echo "<td>".$row['saleType']."</td>";
+                        //Price
                         echo "<td>".'$'.$row['price']."</td>";
+                        //Agent
                         echo "<td>".$row['fname'].' '.$row['lname']."</td>";
                     echo "</tr>";
+                    $count = $count + 1;
                 }
                 echo "</tbody>";
             echo "</table>";
+
+            //Clear variables
             unset($result);
         } else{
-            echo "<div class='alert alert-danger'><em>No entires found.</em></div>";
+            echo "<div class='alert alert-danger'><em>No entries found.</em></div>";
         }
     } else{
         echo "Oops! Something seems to have gone wrong. Please try again later.";
---
+++ admin/new.php
@@ -0,0 +1,227 @@
+<?php
+
+$title = "New Listing"; //The Page Title
+require_once('../includes/layouts/header.php'); //Gets the header
+require_once('../includes/db.php'); //Connect to the database
+
+//Defining our variables
+/*$inputs = array(
+    'saleType' => '',
+    'price' => '',
+    'description' => '',
+    'bedrooms' => '',
+    'bathrooms' => '',
+    'garage' => '',
+    'agent_ID' => '',
+    'streetNum' => '',
+    'street' => '',
+    'city' => '',
+    'postcode' => ''
+);
+
+$errors = array(
+    'saleType_err' => '',
+    'price_err' => '',
+    'description_err' => '',
+    'bedrooms_err' => '',
+    'bathrooms_err' => '',
+    'garage_err' => '',
+    'agent_ID_err' => '',
+    'streetNum_err' => '',
+    'street_err' => '',
+    'city_err' => '',
+    'postcode_err' => ''
+);*/
+
+$saleType = $price = $description = $bedrooms = $bathrooms = $garage = $agent_ID = $streetNum = $street = $city = $postcode = '';
+$saleType_err = $price_err = $description_err = $bedrooms_err = $bathrooms_err = $garage_err = $agent_ID_err = $streetNum_err = $street_err = $city_err  = $postcode_err = '';
+
+//Validate input, passing important variables by reference
+function validateInput($input = '', &$err = '', &$output = '', $errMsg = '') {
+    if (empty($input)){
+        $err = $errMsg;
+
+    } else {
+        $output = $input;
+    }
+}
+
+//Validate input with regular expression check, passing important variables by reference
+function complexValidateInput($input = '', &$err = '', &$output = '', $errMsg = '', $errInvalid = '', $regex = '') {
+    if (empty($input)){
+        $err = $errMsg;
+
+    } elseif (!filter_var($input, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>$regex)))){
+        $err = $errInvalid;
+
+    } else {
+        $output = $input;
+    }
+}
+
+
+//Process form data on submit
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $input_agent = trim($_POST["agent"]);
+    //Validate assigned agent
+    //complexValidateInput($input_agent, $agent_ID_err, $agent_ID, "Please select an agent to be assigned to this listing", "Please select an agent to be assigned to this listing", "/^[0-9]*$/");
+    complexValidateInput($input_agent, $agent_ID_err, $agent_ID, "Please select an agent to be assigned to this listing", "Please select an agent to be assigned to this listing", "/^[0-9]*$/");
+
+    $input_streetNum = trim($_POST["streetNum"]);
+    //Validate street number
+    complexValidateInput($input_streetNum, $streetNum_err, $streetNum, "Please enter a street number", "Please enter a valid street number", "/^[a-zA-Z0-9]*$/");
+
+
+    $input_street = trim($_POST["street"]);
+    //Validate street
+    validateInput($input_street, $street_err, $street, "Please enter a street");
+
+
+    $input_city = trim($_POST["city"]);
+    //Validate city
+    validateInput($input_city, $city_err, $city, "Please enter a city");
+
+
+    $input_postcode = trim($_POST["postcode"]);
+    //Validate postcode
+    complexValidateInput($input_postcode, $postcode_err, $postcode, "Please enter a postcode", "Please enter a valid postcode", "/^[0-9]*$/");
+
+
+    $input_bedrooms = trim($_POST["bedrooms"]);
+    //validate bedrooms
+    complexValidateInput($input_bedrooms, $bedrooms_err, $bedrooms, "Please enter the number of bedrooms", "Please enter a number from 0-99", "/^[0-9]*$/");
+
+
+    $input_bathrooms = trim($_POST["bathrooms"]);
+    //validate bedrooms
+    complexValidateInput($input_bathrooms, $bathrooms_err, $bathrooms, "Please enter the number of bathrooms", "Please enter a number from 0-99", "/^[0-9]*$/");
+
+
+    $input_garage = trim($_POST["garage"]);
+    //validate bedrooms
+    complexValidateInput($input_garage, $garage_err, $garage, "Please enter the number of parking spaces", "Please enter a number from 0-99", "/^[0-9]*$/");
+
+    $input_saleType = trim($_POST["saleType"]);
+    //validate sale type
+    validateInput($input_saleType, $saleType_err, $saleType, "Please enter the type of sale");
+
+    $input_price = trim($_POST["price"]);
+    //validate price
+    complexValidateInput($input_price, $price_err, $price, "Please enter a price for the property", "Please enter a valid price", "/^[0-9]*$/");
+
+    $input_description = trim($_POST["description"]);
+    //validate description
+    validateInput($input_description, $description_err, $description, "Please enter a description for the property");
+}
+
+
+?>
+
+<div style="padding-top:70px; padding-bottom:20px;">
+    <div>
+    <?php
+        //If we can retrieve our agent list
+        $sql = "SELECT agent_ID, fname, lname FROM agent";
+        if ($result = $pdo->query($sql)){
+            if ($result->rowCount() > 0){
+
+            } else {
+                echo "No agents to retrieve. Try creating one first.";
+            }
+        } else {
+            echo "Unable to retrieve agents. Something went wrong. Please try again later.";
+        }
+    ?>
+    </div>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="row">
+            <div class="form-group col">
+                <label for="agent" style="display:block">Assigned Agent</label>
+                <select name="agent" id="agent" class="form-control <?php echo (!empty($agent_ID_err)) ? 'is-invalid' : ''; ?>">
+                    <option value="" <?php if (!isset($agent_ID)){ echo "selected"; } ?>>Select an Agent</option>
+                    <?php
+                    //Generate dropdown options from agents table
+                    while ($row = $result->fetch()){
+                        if ($row['agent_ID'] == $agent_ID){
+                            echo '<option selected value="' . $row['agent_ID'] . '">' . $row['fname'] . ' ' . $row['lname'] . '</option>';
+                        } else {
+                        echo '<option value="' . $row['agent_ID'] . '">' . $row['fname'] . ' ' . $row['lname'] . '</option>';
+                        }
+                    }
+                    ?>
+                </select>
+                <span class="invalid-feedback"><?php echo $agent_ID_err;?></span>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="form-group col-md-2">
+                <label for="streetNum">Number</label>
+                <input type="text" id="streetNum" name="streetNum" class="form-control <?php echo (!empty($streetNum_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $streetNum ?>">
+                <span class="invalid-feedback"><?php echo $streetNum_err;?></span>
+            </div>
+            <div class="form-group col">
+                <label for="street">Street</label>
+                <input type="text"  id="street" name="street" maxlength="100" class="form-control <?php echo (!empty($street_err)) ? 'is-invalid' : ''; ?>"value=" <?php echo $street ?>">
+                <span class="invalid-feedback"><?php echo $street_err;?></span>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="city">City</label>
+                <input type="text" id="city" name="city" maxlength="100" class="form-control <?php echo (!empty($city_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $city ?>">
+                <span class="invalid-feedback"><?php echo $city_err;?></span>
+            </div>
+            <div class="form-group col-md-2">
+                <label for="postcode">Postcode</label>
+                <input type="text" id="postcode" name="postcode" maxlength="4" class="form-control <?php echo (!empty($postcode_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $postcode ?>">
+                <span class="invalid-feedback"><?php echo $postcode_err;?></span>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="form-group col-md">
+                <label for="bedrooms">Bedrooms</label>
+                <input type="text" id="bedrooms" name="bedrooms" maxlength="2" class="form-control <?php echo (!empty($bedrooms_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $bedrooms ?>">
+                <span class="invalid-feedback"><?php echo $bedrooms_err;?></span>
+            </div>
+            <div class="form-group col-md">
+                <label for="bathrooms">Bathrooms</label>
+                <input type="text" id="bathrooms" name="bathrooms" maxlength="2" class="form-control <?php echo (!empty($bathrooms_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $bathrooms ?>">
+                <span class="invalid-feedback"><?php echo $bathrooms_err;?></span>
+            </div>
+            <div class="form-group col-md">
+                <label for="garage">Parking</label>
+                <input type="text" id="garage" name="garage" maxlength="2" class="form-control <?php echo (!empty($garage_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $garage ?>">
+                <span class="invalid-feedback"><?php echo $garage_err;?></span>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="form-group col-md">
+                <label for="saleType">Sale Type</label>
+                <input type="text" id="saleType" name="saleType" maxlength="20" class="form-control <?php echo (!empty($saleType_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $saleType ?>">
+                <span class="invalid-feedback"><?php echo $saleType_err;?></span>
+            </div>
+            <div class="form-group col-md">
+                <label for="price">Price</label>
+                <input type="text" id="price" name="price" class="form-control <?php echo (!empty($price_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $price ?>">
+                <span class="invalid-feedback"><?php echo $price_err;?></span>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control <?php echo (!empty($description_err)) ? 'is-invalid' : ''; ?>" name="description" id="description"><?php echo $description; ?></textarea>
+                <span class="invalid-feedback"><?php echo $description_err;?></span>
+            </div>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+
+<?php
+require_once('../includes/layouts/footer.php'); //Gets the footer
+?>
