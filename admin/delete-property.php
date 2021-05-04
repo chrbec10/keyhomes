513f5e391796461e9765adafd9c19aadcd732d40
keybecker <82342623+keybecker@users.noreply.github.com>
2021-05-04 13:22:58 +1200

    Display address when deleting property
    delete-property page now shows the address of the record you are trying to delete, and appropriately redirects to 404 when trying to delete a record that doesn't exist

--- admin/delete-property.php
+++ admin/delete-property.php
@@ -4,7 +4,30 @@ $title = "Delete Property"; //The Page Title
 require_once('../includes/layouts/header.php'); //Gets the header
 require_once('../includes/db.php'); //Connect to the database
 
+$address = '';
 
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    //Prepare an SQL statment
+    $sql = "SELECT * FROM property WHERE property_ID = :id";
+
+    if ($stmt = $pdo->prepare($sql)){
+        //Bind variables
+        $stmt->bindParam(":id", $param_ID);
+        $param_ID = trim($_GET['id']);
+
+        //Attempt query
+        if ($stmt->execute()){
+            if ($stmt->rowCount() == 1){
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                $address = $row['streetNum'] . ' ' . $row['street'] . ', ' . $row['city'] . ' ' . $row['postcode'];
+
+            } else {
+                header("location: ../404.php");
+            }
+        }  
+    }
+}
 
 if(isset($_POST['id']) && !empty($_POST['id'])){
     //Prepare an SQL statement
@@ -12,8 +35,8 @@ if(isset($_POST['id']) && !empty($_POST['id'])){
 
     if ($stmt = $pdo->prepare($sql)){
         //Bind variables
-        $stmt->bindParam(":id", $param_id);
-        $param_id = trim($_POST['id']);
+        $stmt->bindParam(":id", $param_ID);
+        $param_ID = trim($_POST['id']);
 
         //Attempt to execute the prepared statement
         if($stmt->execute()){
@@ -35,15 +58,19 @@ if(isset($_POST['id']) && !empty($_POST['id'])){
         exit();
     }
 }
+
+
 ?>
+
 <div class="content-top-padding pb-4 bg-light">
     <div class="container mt-4">
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="alert alert-danger">
                 <input type="hidden" name="id" id="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                <p>Are you sure you want to delete this record?</p>
+                <p>Are you sure you want to delete the record for <?php echo $address; ?>?</p>
                 <p>This action cannot be undone</p>
-                <p><input type="submit" value="Delete" class="btn btn-danger"><a href="index.php" class="btn btn-secondary ml-2">Cancel</a></p>
+                <p><input type="submit" value="Delete" class="btn btn-danger">
+                <a href="index.php" class="btn btn-secondary ml-2">Cancel</a></p>
             </div>
         </form>
     </div>
